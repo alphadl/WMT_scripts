4222a2b06b619b5d9ee5ff701d989659f36f7475
Liam <liangding.liam@gmail.com>
2019-04-18 16:45:18 +1000

    Update fi2en_data.sh

--- train/fi2en_data.sh
+++ train/fi2en_data.sh
@@ -13,7 +13,7 @@ NUM=5000
 
 mkdir -p fi2en
 
-python /project/LIAM/work/WMT19/code/OpenNMT-py/preprocess.py \
+python preprocess.py \
 -train_src ${SRC} -train_tgt ${TGT} \
 -valid_src newstest2018-fien-src.true.fi.bpe -valid_tgt newstest2018-fien-ref.true.en.bpe \
 -save_data fi2en/fi2en \
